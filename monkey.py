086f8399d666a05e1232934c17e92de1b7ba0512
zhulixin <lixin.zhu@quvideo.com>
2018-09-12 16:00:33 +0800

    新增crashreport解析

--- monkey.py
+++ monkey.py
@@ -2,6 +2,14 @@
 
 import os
 import time
+import subprocess
+from iOSCrashAnalysis import FileOperate
+
+
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+
 
 def dirlist(path, allfile):
     filelist = os.listdir(path)
@@ -26,7 +34,8 @@ def get_ipa(path,file_format):
 
 def install(path,file_format,duid):
     ipa_path = get_ipa(path,file_format)
-    cmd = 'ios-deploy –r -b ' + '"' + ipa_path +'"' + ' -i ' + duid
+    cmd = 'ios-deploy –r -b ' + '"' + ipa_path + '"' + ' -i ' + duid
+    print('安装待测试的app', cmd)
     try:
         os.system(cmd)
     except Exception as msg:
@@ -34,7 +43,7 @@ def install(path,file_format,duid):
         raise
 
 def monkey(devicename):
-    cmd_monkey = "xcodebuild -project /Users/zhulixin/Desktop/Fastmonkey/XCTestWD-master/XCTestWD/XCTestWD.xcodeproj " \
+    cmd_monkey = "xcodebuild -project /Users/xxxx/Desktop/iOS-monkey/XCTestWD-master/XCTestWD/XCTestWD.xcodeproj " \
                  "-scheme XCTestWDUITests " \
                  "-destination 'platform=iOS,name=" + devicename + "' " + \
                  "XCTESTWD_PORT=8001 " + \
@@ -47,16 +56,81 @@ def monkey(devicename):
         print('error message:', msg)
         raise
 
+
+# def monkey():
+#     cmd_path = 'cd /Users/iOS_Team/.jenkins/workspace/iOS_Monkey_VivaVideo/AutoMonkey4IOS'
+#     cmd_monkey = './start_monkey.sh'
+
+#     print(cmd_path)
+
+#     try:
+#         os.system(cmd_path)
+#     except Exception as msg:
+#         print('error message:', msg)
+#         raise
+
+#     time.sleep(3)
+
+#     print(cmd_monkey)
+#     try:
+#         # os.system(cmd_monkey)
+#         subprocess.run(cmd_monkey, shell=True)
+#         # monkeylog = open('/Users/iOS_Team/QA/Monkey/AutoMonkey4IOS/output/' + 'monkeylog.txt', 'w')
+#         # subprocess.Popen(cmd_monkey, shell=True, stdout=monkeylog)
+#     except Exception as msg:
+#         print('error message:', msg)
+#         raise
+
+
 if __name__ == '__main__':
-    path = "/Users/iOS_Team/XiaoYing_AutoBuild/XiaoYing/XiaoYingApp/"
+    path = "/Users/iOS_Team/XiaoYing_AutoBuild/XiaoYing/XiaoYingApp/fastlane/"
     file_format = ['.ipa']
-    duid = '5214866ccb9342f87f4c2aab093c25f7e252fd85'
-    devicename = '6s2050'
-    # ipa = get_ipa(path,file_format)
-    # print(ipa)
-    # install(path,file_format,duid)
+    duid = '4c4ebeb44e5312c50c54b274a9145ebf89dce686'
+    devicename = '5s2094'
+    ipa = get_ipa(path, file_format)
+    print(ipa)
+    install(path, file_format, duid)
     print("start monkey")
     monkey(devicename)
+
+    print("============开始导出crashreport==========")
+    find_str = 'XiaoYing-'  # 待测app crashreport文件关键字
+    file_format1 = [".ips"]  # 导出的crash文件后缀
+    file_format2 = [".crash"]  # 解析后的crash文件后缀
+
+    reportPath = PATH("/Users/xxxx/Downloads/iOS_Monkey_VivaVideo/AutoMonkey4IOS/CrashInfo/")
+    beforePath = os.path.join(reportPath + '/Before')
+    if not os.path.exists(beforePath):
+        os.makedirs(beforePath)
+
+    afterPath = os.path.join(reportPath + '/After')
+    if not os.path.exists(afterPath):
+        os.makedirs(afterPath)
+
+    # 导出设备中的所有crash文件
+    exportReport = 'idevicecrashreport -u ' + duid + ' ' + beforePath + '/'
+    print(exportReport)
+    os.system(exportReport)  # 导出设备中的crash
+
+    print("============开始过滤并解析待测app相关crashreport==========")
+    f = FileOperate.FileFilt()
+    f.FindFile(find_str, file_format1, beforePath)
+    for file in f.fileList:
+        inputFile = os.path.abspath(file)  # 绝对路径
+        # print(inputFile)
+        analysisPath = PATH("/Users/xxxx/Desktop/iOS-monkey/iOSCrashAnalysis/")
+        cmd_export = 'export DEVELOPER_DIR="/Applications/XCode.app/Contents/Developer"'
+        cmd_analysis = 'python3 ' + analysisPath + '/BaseIosCrash.py' + ' -i ' + inputFile
+        # print(cmd_analysis)
+        os.system(cmd_analysis)
+
+    # 移动解析完成的crashreport到新的文件夹
+    f.MoveFile(find_str, file_format2, beforePath, afterPath)
+    print("============crashreport解析完成==========")
+
+    # 删除所有解析之前的crash文件，若不想删除，注掉即可
+    print("============删除所有解析之前的crash文件==========")
+    f.DelFolder(beforePath)
 
 
 
